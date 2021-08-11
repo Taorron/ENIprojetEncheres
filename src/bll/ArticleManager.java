fb0735c78230d7ca5da1afbd58a2b81cc86dff86
Neriondiel <cyril.18.09.97@hotmail.fr>
2021-08-11 17:55:09 +0200

    ArticleManager : getArticles & SetInfo

--- src/bll/ArticleManager.java
+++ src/bll/ArticleManager.java
@@ -1,9 +1,11 @@
 package bll;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bo.ArticleVendu;
 import dal.ArticleDAO;
+import dal.DALException;
 import dal.DAOFactory;
 
 public class ArticleManager {
@@ -20,10 +22,27 @@ public class ArticleManager {
 		return instance;
 	}
 	
+	public List<ArticleVendu> getArticles(){
+		List<ArticleVendu> result = new ArrayList<ArticleVendu>();
+		
+		try {
+			result = articleDao.select(null, null, null, null, null, null, null, null, null);
+			setInfo(result);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		return result;
+	}
+	
 	public void setInfo(List<ArticleVendu> articles){
 		UserManager userManager = new UserManager();
+		CategoryManager categoryManager = new CategoryManager();
 		for (ArticleVendu articleVendu : articles) {
-			//TODO : Set Category, Retrait, Acheteur si il existe
+			//TODO : Set Retrait et Acheteur si il existe
+			articleVendu.setCategorie(categoryManager.getCategoryById(articleVendu.getCategorie().getNoCategorie()));
 			articleVendu.setVendeur(userManager.getUserById(articleVendu.getVendeur().getNoUtilisateur()));
 		}
 	}
