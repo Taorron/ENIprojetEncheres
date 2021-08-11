b73e85c55e469e822937ceff6a6e991bd6a5074a
Neriondiel <cyril.18.09.97@hotmail.fr>
2021-08-11 16:40:55 +0200

    Add ArticleManager

---
+++ src/bll/ArticleManager.java
@@ -0,0 +1,30 @@
+package bll;
+
+import java.util.List;
+
+import bo.ArticleVendu;
+import dal.ArticleDAO;
+import dal.DAOFactory;
+
+public class ArticleManager {
+
+	private ArticleDAO articleDao = DAOFactory.getArticleDAO();
+	private static ArticleManager instance;
+	
+	//Singleton
+	public static ArticleManager getInstance() throws BLLException{
+		
+		if(instance == null) {
+			instance = new ArticleManager();
+		}
+		return instance;
+	}
+	
+	public void setInfo(List<ArticleVendu> articles){
+		UserManager userManager = new UserManager();
+		for (ArticleVendu articleVendu : articles) {
+			
+			articleVendu.setVendeur(userManager.getUtilisateurById(articleVendu.getVendeur().getNoUtilisateur()));
+		}
+	}
+}
