b567841b90a8b70fe0990c6ba20c42ce307fba63
Neriondiel <cyril.18.09.97@hotmail.fr>
2021-08-16 16:52:50 +0200

    VoirProfilServlet : See seller profil

--- src/controllers/VoirProfilServlet.java
+++ src/controllers/VoirProfilServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bll.UserManager;
 import bo.Utilisateur;
 
 /**
@@ -31,6 +33,29 @@ public class VoirProfilServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String userParameter = request.getParameter("sellerId");
+		HttpSession session = request.getSession();
+		Utilisateur user = (Utilisateur) session.getAttribute("user");
+		boolean canEditProfil = false;
+		Utilisateur seller = null;		
+		
+		if (userParameter != null) {
+			Integer id = Integer.parseInt(request.getParameter("sellerId"));
+			UserManager um = new UserManager();
+			seller = um.getUserById(id);
+			//Si user connecté passe par la methode get pour voir son profil
+			//il peut le modifier
+			if(user != null) {
+				canEditProfil = (id == user.getNoUtilisateur()) ? true : false;
+			}
+			
+		} else {
+			seller = user;
+			canEditProfil = true;
+		}
+		
+		request.setAttribute("canEdit", canEditProfil);
+		request.setAttribute("seller", seller);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/profil.jsp");
 		rd.forward(request, response);
